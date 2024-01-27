34528baffc33a6518e50bc5d5aee0f2ea38f39d3
Farel Ristova <muhammadfarell02rk@gmail.com>
2024-01-26 23:04:59 -0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+// Toogle and Responsive Navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
+// Clear form before unload
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
+
+// HADIAH
+function openGift() {
+    document.querySelector('.explosion').style.display = 'block';
+    setTimeout(function () {
+        alert("Terimakasih sudah datang");
+        document.querySelector('.explosion').style.display = 'none';
+    }, 500);
+}
